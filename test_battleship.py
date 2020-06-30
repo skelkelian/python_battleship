83ab4bc14be5c367ec2c89b9029a7f3682a4ff4e
skelkelian <skelkelian88@gmail.com>
2020-06-29 18:36:16 -0700

    feature: reads config.ini file for user input on the opponent_type

--- test_battleship.py
+++ test_battleship.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import unittest
 from battleship import BattleShip
+from configparser import ConfigParser
 
 class TestInit(unittest.TestCase):
     def test_player_board_1(self):
@@ -23,6 +24,20 @@ class TestInit(unittest.TestCase):
 
         # assert
         self.assertEqual(self.battleship.primary_board_player_one, expected)
+
+    def test_read_computer_config_file(self):
+        # given
+        config = ConfigParser()
+        config.read('config.ini')
+
+        # when
+        expected_opponent_type = 1
+        observed_opponent_type = int(config.get('main', 'opponent_type'))
+
+        # assert
+        self.assertEqual(observed_opponent_type, expected_opponent_type)
+
+
 
 # class TestBattleShip(unittest.TestCase):
     # def setUp(self):
