6254922e881976f2518c2a680ee988a63ce65d4f
skelkelian <skelkelian88@gmail.com>
2021-01-18 13:27:39 -0800

    Created test_player and refactored test_player_board_one into it

---
+++ test_player.py
@@ -0,0 +1,32 @@
+import unittest
+import utils
+from player import Player
+from unittest.mock import patch
+from configparser import ConfigParser
+
+
+class TestPlayer(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_player_board_player_one(self):
+        # given
+        expected_player_board_player_one = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+
+        # when
+        self.player = Player(config_name='config_easy_difficulty.ini')
+
+        # assert
+        self.assertEqual(expected_player_board_player_one, self.player.primary_board_player_one)
+
