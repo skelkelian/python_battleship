043e44bb1b0361d50077c8b7f1c05952730d857e
skelkelian <skelkelian88@gmail.com>
2021-03-13 09:21:13 -0800

    Added test_carrier to runner.py

--- runner.py
+++ runner.py
@@ -4,6 +4,7 @@ import unittest
 # import your test modules
 import test_player
 import test_computer
+import test_carrier
 
 # initialize the test suite
 loader = unittest.TestLoader()
@@ -12,6 +13,7 @@ suite = unittest.TestSuite()
 # add tests to the test suite
 suite.addTests(loader.loadTestsFromModule(test_player))
 suite.addTests(loader.loadTestsFromModule(test_computer))
+suite.addTests(loader.loadTestsFromModule(test_carrier))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
