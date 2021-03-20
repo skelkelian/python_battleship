c9cb64f827801f699fbeae85b01b5417993b37ee
skelkelian <skelkelian88@gmail.com>
2021-03-20 16:36:28 -0700

    Added test_ship to runner.py

--- runner.py
+++ runner.py
@@ -9,6 +9,7 @@ import test_cruiser
 import test_destroyer
 import test_battleship
 import test_patrol_boat
+import test_ship
 
 # initialize the test suite
 loader = unittest.TestLoader()
@@ -22,6 +23,7 @@ suite.addTests(loader.loadTestsFromModule(test_cruiser))
 suite.addTests(loader.loadTestsFromModule(test_destroyer))
 suite.addTests(loader.loadTestsFromModule(test_battleship))
 suite.addTests(loader.loadTestsFromModule(test_patrol_boat))
+suite.addTests(loader.loadTestsFromModule(test_ship))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
