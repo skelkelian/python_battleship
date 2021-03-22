a83f59aa1051a068568532a60e91d9f8df3ebd0d
skelkelian <skelkelian88@gmail.com>
2021-03-22 16:41:31 -0700

    Removed primary_board_computer from ship class

--- ship.py
+++ ship.py
@@ -3,18 +3,4 @@ from random import randint
 
 class Ship:
     def __init__(self):
-        self.primary_board_computer = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ]
-
-    def get_primary_board_computer(self):
-        return self.primary_board_computer
+        pass
