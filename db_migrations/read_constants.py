7fe0f1fe133cf8d8f52b27b3a8fd2b1efd5306b5
skelkelian <skelkelian88@gmail.com>
2021-04-02 16:05:08 -0700

    recovered file read_constants.py and put it in db_migrations

---
+++ db_migrations/read_constants.py
@@ -0,0 +1,30 @@
+import psycopg2
+import sys
+
+con = None
+
+try:
+    con = psycopg2.connect("host='localhost' dbname='skelkelian'")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM constants")
+
+    while True:
+        row = cur.fetchone()
+
+        if row == None:
+            break
+
+        constant = row[0]
+        value = row[1]
+        print("constant: {}\nvalue: {}\n\n".format(constant, value))
+
+except psycopg2.OperationalError as ex:
+    if con:
+        con.rollback()
+
+    print("Connection failed: {0}".format(ex))
+    sys.exit(1)
+
+finally:
+    if con:
+        con.close()
